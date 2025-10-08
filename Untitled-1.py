f8311ff482c695855c0178c6d84c3c6fd8eebdb1
unknown <acastilloh5@unemi.edu.ec>
2025-10-07 22:28:19 -0500

    mi primer commit

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+# Un código muy sencillo para sumar dos números
+
+# 1. Definimos las variables (Expresiones algebraicas simples)
+x = 10
+y = 5
+
+# 2. Realizamos la operación (la ecuación/expresión a resolver)
+resultado = x + y
+
+# 3. Imprimimos el resultado
+print(resultado)
